ec7b56c71c91176e213e617b171a8b294ad89fb2
Noel Vock <noel1uk@gmail.com>
2019-04-13 22:21:42 +0100

    ngFor example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+
+  policies = [
+    {id: 0, name: 'p1'},
+    {id: 2, name: 'p2'},
+  ];
   title = 'ng-for';
 }
+
+// export interface Policies {
+//   id: number;
+//   name: string;
+// }
